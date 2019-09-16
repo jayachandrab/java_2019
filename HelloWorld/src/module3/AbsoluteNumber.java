78ea76f79bb24bb5fb023af6b13c1f74fb983c92
Jayachandra <jayachandra1805@gmail.com>
2019-09-16 11:20:43 +0530

    Module3

---
+++ HelloWorld/src/module3/AbsoluteNumber.java
@@ -0,0 +1,27 @@
+package module3;
+
+/**
+ * Given n, find the absolute value of n.
+ * 
+ * Input : abs(-1)
+ * Output : 1
+ * 
+ * Input : abs(0)
+ * Output : 0
+ * 
+ * @author Siva Sankar
+ */
+
+public class AbsoluteNumber {
+
+    /**
+     * This method returns the absolute number of the given n
+     * @param n, the integer
+     * @return absolute value of n
+     */
+    public static int abs(int n) {
+        // Your code goes here....
+    	System.out.println("hi");
+    	return Math.abs(n);
+    }
+}
