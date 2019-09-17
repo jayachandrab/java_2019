ab7699a610aaf20504b82cf559082e75a2eae890
Jayachandra <jayachandra1805@gmail.com>
2019-09-17 10:54:05 +0530

    Assignment4

---
+++ HelloWorld/src/assignment4/EvenOrOdd.java
@@ -0,0 +1,22 @@
+package assignment4;
+
+/**
+ * Given n, return true if n is even otherwise false.
+ * 
+ * @author Siva Sankar
+ */
+
+public class EvenOrOdd {
+
+    /**
+     * This method returns true if it is an even number false otherwise.
+     * @param n, the integer
+     * @return true if n is even and false otherwise.
+     */
+    public static boolean evenOrOdd(int n) {
+        //  Your code goes here....
+        //
+        System.out.println("checking odd or even");
+        return n%2==0;
+    }
+}
