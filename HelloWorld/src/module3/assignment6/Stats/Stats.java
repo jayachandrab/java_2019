61e7314d8a0dfc7f9052fb853a97b1584a0319da
Jayachandra <jayachandra1805@gmail.com>
2019-09-18 14:37:04 +0530

    Module 2 assigment 6

---
+++ HelloWorld/src/module3/assignment6/Stats/Stats.java
@@ -0,0 +1,148 @@
+package module3.assignment6.Stats; /**
+ * Given an array of integers Find the mean, median, mode, standard deviation
+ * and variance.
+ * 
+ * Mean
+ * Input : [1,2,3,4,5]
+ * Output : 3.0
+ * 
+ * Median
+ * Input : [1,2,3,4,5]
+ * Output : 3.0
+ * 
+ * Mode
+ * Input : [1,1,2,2,3]
+ * Output : [1,2]
+ * 
+ * Variance
+ * Input : [1,2,3,4,5]
+ * Output : 2.0
+ * 
+ * Standard Deviation
+ * Input : [1,2,3,4,5]
+ * Output : 1.41421356
+ * 
+ * @author Siva Sankar
+ */
+
+import java.util.*;
+
+public class Stats {
+
+    /**
+     * This method should return the mean of the elements of the array.
+     * If the arr is empty (length is 0), return the mean as 0.
+     * @param arr, the input array contians elements.
+     * @return the mean of the array.
+     */
+    public static double mean(int[] arr) {
+        //  Your code goes here....
+        if(arr.length==0){
+            return 0;
+        }
+        int sum = 0;
+        for (int i = 0; i < arr.length; i++) {
+            sum += arr[i];
+        }
+
+        return (double)sum / (double)arr.length;
+
+    }
+
+    /**
+     * This method returns the median of the elements of the array.
+     * Prerequisite : The array should be sorted to find the median.
+     * 
+     * Hint : Arrays.sort(arr) sorts the elements of the array.
+     * 
+     * @param arr, the input array contains random elements (not in sorted order)
+     * @return the median of the elements of the array.
+     */
+    public static double median(int[] arr) {
+        //  Your code goes here...
+        Arrays.sort(arr);
+
+        // check for even case
+        if (arr.length % 2 != 0)
+            return (double)arr[arr.length / 2];
+
+        return (double)(arr[(arr.length - 1) / 2] + arr[arr.length / 2]) / 2.0;
+
+    }
+
+    /**
+     * This method returns the mode of the array. If there is no mode, then it returns
+     * 0 as the mode.
+     * 
+     * @param arr, the input array.
+     * @return the mode of the array, 0 otherwise if there is no mode.
+     */
+    public static int mode(int[] arr) {
+        //  Your code goes here....
+        int maxValue = 0, maxCount = 0;
+        int c=0;
+        for (int i = 0; i < arr.length; ++i)
+        {
+            int count = 0;
+            c=0;
+            for (int j = 0; j < arr.length; ++j)
+            {
+                if (arr[j] == arr[i])
+                {
+                    c=c+1;
+                    ++count;
+                }
+
+            }
+            if (count > maxCount)
+            {
+                maxCount = count;
+                maxValue = arr[i];
+            }
+        }
+        System.out.println(c);
+        if(c==1){
+            return 0;
+        }
+        return maxValue;
+    }
+
+    /**
+     * This method returns the variance of the input array.
+     * Make use of the mean functon which has already written.
+     * 
+     * @param arr, the input array
+     * @return the variance of the array.
+     */
+    public static double variance(int[] arr) {
+        //  Your code goes here....
+
+        double sum = 0;
+
+        for (int i = 0; i < arr.length; i++)
+            sum += arr[i];
+        double mean = (double)sum /
+                (double)arr.length;
+
+        // Compute sum squared
+        // differences with mean.
+        double sqDiff = 0;
+        for (int i = 0; i < arr.length; i++)
+            sqDiff += (arr[i] - mean) *
+                    (arr[i] - mean);
+
+        return (double)sqDiff / arr.length;
+    }
+
+    /**
+     * This method returns the standard deviation of the array.
+     * Hint : You can find the square root using Math.sqrt() method.
+     * @param arr, the input array
+     * @return the standard deviation of the input array.
+     */
+    public static double standardDeviation(int[] arr) {
+        //  Your code goes here....
+
+        return Math.sqrt(variance(arr));
+    }
+}
