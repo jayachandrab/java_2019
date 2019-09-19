593ebb3721e0adc87d0425254cf7a80f9df9090b
Jayachandra <jayachandra1805@gmail.com>
2019-09-19 15:07:57 +0530

    Module 4 assignment 2 added more functionality

--- HelloWorld/src/moduler4/assignment2/CardCatalog.java
+++ HelloWorld/src/moduler4/assignment2/CardCatalog.java
@@ -5,17 +5,28 @@ public class CardCatalog {
     String author[];
     String subject[];
    static int count=0;
+   Card cards[];
     CardCatalog(){
         this.title=new String[10];
         this.author=new String[10];
         this.subject=new String[10];
+        this.cards=new Card[10];
     }
-    public void  addACard(){
-
+    public void  addACard(Card card){
+        title[count]=card.title;
+        author[count]=card.author;
+        subject[count]= card.subject;
+        cards[count]=card;
+        count++;
     }
 
-    public void getATitle(){
-
+    public Card getATitle(String title){
+        for(int i=0;i<cards.length;i++){
+            if(cards[i].getTitle().equals(title)){
+                return cards[i];
+            }
+        }
+        return null;
     }
     public void getAnAuthor(){
 
--- HelloWorld/src/moduler4/assignment2/LibraryCatalog.java
+++ HelloWorld/src/moduler4/assignment2/LibraryCatalog.java
@@ -1,4 +1,41 @@
 package moduler4.assignment2;
 
+import java.util.Scanner;
+
 public class LibraryCatalog {
+
+    public static void main(String args[]){
+
+        Scanner in=new Scanner(System.in);
+        CardCatalog card=new CardCatalog();
+
+        while(true){
+            System.out.println("Enter your choice 1 for add card\n 2 get card by title \n 3 for exit");
+            int choice=in.nextInt();
+
+            switch (choice){
+                case 1:
+                    System.out.println("enter book title");
+                    String title=in.next();
+                    System.out.println("enter book author");
+                    String author=in.next();
+                    System.out.println("enter book subject");
+                    String subject=in.next();
+                    Card book=new Card(title,author,subject);
+
+                    card.addACard(book);
+                    break;
+                case 2:
+                    System.out.println("Enter title");
+
+                    title=in.next();
+                    System.out.println(card.getATitle(title));
+                    break;
+                case 3:
+                    System.exit(1);
+                default:
+                    System.out.println("Please choice right option");
+            }
+        }
+    }
 }
